cc35fd254854ab20ccd59d0885a67696bddf324b
Tashman91 <iainlumsden1991@gmail.com>
2021-12-13 17:34:45 +0000

    movement still not working

--- Elwynds Grand Adventure/Elwynds Grand Adventure/Assets/Scripts/PlayerController.cs
+++ Elwynds Grand Adventure/Elwynds Grand Adventure/Assets/Scripts/PlayerController.cs
@@ -12,19 +12,31 @@ public class PlayerController : MonoBehaviour
 
     Vector2 movement;
 
+    public bool grounded;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadious;
+
+
     void Start()
     {
-        
+        rB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadious, whatIsGround);
+
+        if(grounded == true)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+      
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         rB.MovePosition(rB.position * movement * moveSpeed * Time.fixedDeltaTime);
     }
